24a94cf73c112ec904d4e1aa2ce38d1fe09b42ca
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-18 16:24:26 +0900

    Android I
    環境設定
    
    7/18 Finish

---
+++ app/src/main/java/jp/ac/it_college/std/s23006/listviewsample2/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.ac.it_college.std.s23006.listviewsample2
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23006.listviewsample2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
