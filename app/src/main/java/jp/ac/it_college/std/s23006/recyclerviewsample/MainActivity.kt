7e3dd5d51249e7e29853abffcc5dc357a24c4fb0
itc-s23006 <s23006@std,it-college.ac.jp>
2024-07-24 16:38:20 +0900

    Android I
    FirstCommit
    
    7/24

---
+++ app/src/main/java/jp/ac/it_college/std/s23006/recyclerviewsample/MainActivity.kt
@@ -0,0 +1,24 @@
+package jp.ac.it_college.std.s23006.recyclerviewsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23006.recyclerviewsample.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.main)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
