6871f90871f585e95369adba02b543c2768e6fe5
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-25 09:45:05 +0900

    Android I
    6.4 完了
    
    7/24 Finish

--- app/src/main/java/jp/ac/it_college/std/s23006/constraintlayoutsample/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/constraintlayoutsample/MainActivity.kt
@@ -11,11 +11,13 @@ import jp.ac.it_college.std.s23006.constraintlayoutsample.databinding.ActivityMa
 import jp.ac.it_college.std.s23006.constraintlayoutsample.databinding.ActivitySecondBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityMainBinding
+    // private lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivitySecondBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
+        // binding = ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivitySecondBinding.inflate(layoutInflater)
         enableEdgeToEdge()
         setContentView(binding.root)
         ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
