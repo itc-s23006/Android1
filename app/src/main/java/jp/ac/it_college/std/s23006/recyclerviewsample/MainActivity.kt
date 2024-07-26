988c40ff9363612250c9d89a80e4f94f14c4341e
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-26 11:12:32 +0900

    Android I
    レイアウトの準備、できた。&
    データの表示まで。
    
    7/25 Finish

--- app/src/main/java/jp/ac/it_college/std/s23006/recyclerviewsample/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/recyclerviewsample/MainActivity.kt
@@ -5,7 +5,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
 import jp.ac.it_college.std.s23006.recyclerviewsample.databinding.ActivityMainBinding
+import kotlinx.serialization.json.Json
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
@@ -19,6 +22,21 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val jsonString = resources.openRawResource(R.raw.set_meal).reader().readText()
+
+        val setMealList = Json.decodeFromString<List<SetMeal>>(jsonString)
+
+        binding.setMealList.apply {
+            val linearLayoutManager = LinearLayoutManager(this@MainActivity)
+            layoutManager = linearLayoutManager
+
+            val setMealAdapter = SetMealAdapter(setMealList)
+            adapter = setMealAdapter
+
+            addItemDecoration(
+                DividerItemDecoration(this@MainActivity, linearLayoutManager.orientation)
+            )
         }
     }
 }
