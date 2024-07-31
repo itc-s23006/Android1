e517f425e5a4b4fadec7e1f22db74b6fd7341e34
itc-s23006 <s23006@std.it-college.ac.jp>
2024-07-31 15:48:03 +0900

    RecyclerViewSampleを移植　+ 7章完了
    7/31 Finish

---
+++ app/src/main/java/jp/ac/it_college/std/s23006/intelsample/MenuThanksActivity.kt
@@ -0,0 +1,34 @@
+package jp.ac.it_college.std.s23006.intelsample
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23006.intelsample.databinding.ActivityMenuThanksBinding
+
+class MenuThanksActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMenuThanksBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMenuThanksBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val menuName = intent.getStringExtra("menuName")
+        val menuPrice = intent.getIntExtra("menuPrice", 0)
+
+        binding.tvMenuName.text = menuName
+        binding.tvMenuPrice.text = menuPrice.toString()
+
+        binding.btThxBack.setOnClickListener {
+            finish()
+        }
+    }
+}
