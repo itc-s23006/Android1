cc8e60152682732ed521816ce822084fe744ddc3
itc-s23006 <s23006@std.it-college.ac.jp>
2024-08-20 15:27:50 +0900

    14.1 暗黙的インデント 14.2 緯度と軽度の指定で地図アプリを起動するURI
    できた！

--- app/src/main/java/jp/ac/it_college/std/s23006/implicitlntentsample/MainActivity.kt
+++ app/src/main/java/jp/ac/it_college/std/s23006/implicitlntentsample/MainActivity.kt
@@ -1,20 +1,55 @@
 package jp.ac.it_college.std.s23006.implicitlntentsample
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import jp.ac.it_college.std.s23006.implicitlntentsample.databinding.ActivityMainBinding
+import java.net.URLEncoder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    private var latitude = 0.0
+
+    private var longitude = 0.0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(binding.main) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        binding.apply {
+            binding.btMapSearch.setOnClickListener(::onMapSearchButtonClick)
+
+            binding.btMapShowCurrent.setOnClickListener(::onMapShowCurrentButtonClick)
+        }
+    }
+
+    private fun onMapShowCurrentButtonClick(view: View) {
+        val uri = Uri.parse("geo:$latitude,$longitude")
+
+        startActivity(Intent(Intent.ACTION_VIEW, uri))
+    }
+    private fun onMapSearchButtonClick(view: View) {
+        val searchWord = binding.etSearchWord.text.toString().let { keyword ->
+            URLEncoder.encode(keyword, Charsets.UTF_8.name())
+        }
+
+        val uri = Uri.parse("geo:0,0?q=$searchWord")
+
+        Intent(Intent.ACTION_VIEW, uri).let { intent ->
+            startActivity(intent)
         }
     }
 }
